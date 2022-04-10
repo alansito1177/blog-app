0ba460987dbbed89ebb50668cdc018ae7ed09be9
Alan Avellaneda <alansito1177@gmail.com>
2022-04-10 17:42:54 -0400

    Styled Create Post Page

--- src/pages/createPost.js
+++ src/pages/createPost.js
@@ -2,16 +2,21 @@ import React from "react";
 
 function CreatePost() {
   return (
-    <div className="CreatePostPage">
-      <div className="container">
-        <h1>Create a post</h1>
-        <div>
-          <label>Title:</label>
-          <input placeholder="Title..." />
-        </div>
-        <div>
-          <label>Post:</label>
-          <textarea placeholder="post.." />
+    <div className="container p-2">
+      <div className="row m-2 justify-content-center">
+        <div className="card col-md-7 text-center">
+          <div className="card-body">
+            <h1>Create a post</h1>
+            <div>
+              <h3>Title:</h3>
+              <input className="input-group" placeholder="Title..." />
+            </div>
+            <div className="">
+              <h3>Post Body:</h3>
+              <textarea className="input-group" placeholder="post.." />
+            </div>
+            <button className=" input-group btn btn-primary mt-3">Post</button>
+          </div>
         </div>
       </div>
     </div>
